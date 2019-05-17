11d54b00115d1fabc300874ae8b6f633ba84ed41
Doniyor4120 <36728197+Doniyor4120@users.noreply.github.com>
2019-05-17 18:51:48 +0500

    updated

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -4,7 +4,7 @@ import Slide from 'react-reveal/Slide';
 class TimeUntil extends Component {
 
     state = {
-        deadline: 'Dec, 16, 2018',
+        deadline: 'July, 16, 2019',
         days:'0',
         hours:'0',
         minutes:'0',
